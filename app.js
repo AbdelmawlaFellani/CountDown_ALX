b070abebfd2d45b68f9f8c27fd724503454af7b8
Abdelmawla <67197201+AbdelmawlaFellani@users.noreply.github.com>
2023-02-28 19:29:33 +0100

    End of DOM Project #1 but might Edit end date later

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const end_date = new Date("12/15/2023");
-const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+//Constants
+const end_date = new Date("02/20/2024"); // after 1 year of the beginning date of the Program 
 const _MS_PER_MONTHS = 1000 * 60 * 60 * 24 * 30;
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+const _MS_PER_HRS = 1000 * 60 * 60;
+const _MS_PER_MIN = 1000 * 60 ;
+const _MS_PER_SEC = 1000;
+// Elements
+monthBlock = document.getElementById('month');
+dayBlock = document.getElementById('day');
+hrsBlock = document.getElementById('hour');
+minBlock = document.getElementById('minute');
+secBlock = document.getElementById('seconds');
+
+
 setInterval(() => {
-  var curr_date = new Date();
-  document.getElementById("count").innerHTML =
-    Math.ceil(
-      Math.abs(end_date.getTime() - curr_date.getTime()) / _MS_PER_DAY
-    ) + " Days";
+  var curr_date = new Date(); 
+  
+    let total_time = Math.abs(end_date.getTime() - curr_date.getTime());
+
+    monthBlock.innerHTML = Math.floor(total_time / _MS_PER_MONTHS);
+    total_time %= _MS_PER_MONTHS;
+
+    dayBlock.innerHTML = Math.floor(total_time / _MS_PER_DAY);
+    total_time %=_MS_PER_DAY;
+
+    hrsBlock.innerHTML = Math.floor(total_time / _MS_PER_HRS);
+    total_time %= _MS_PER_HRS;
+
+      minBlock.innerHTML = Math.floor(total_time / _MS_PER_MIN);
+    total_time %= _MS_PER_MIN;
+    
+    secBlock.innerHTML = Math.floor(total_time / _MS_PER_SEC); 
+
 }, 1000);
